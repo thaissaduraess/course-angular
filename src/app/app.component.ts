3c6ccf41852f3aad0e8b440786fa778484c1c3f8
thaissa <thaissaduraes67@gmail.com>
2023-08-18 07:02:26 -0300

    subindo curso na segundo modulo, data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'course-angular';
+  valor: number = 5;
+
+  deletarCiclo: boolean = false;
+
+  mudarValor() {
+    this.valor++;
+  }
+
+  destruirClico() {
+    this.deletarCiclo = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataBindingComponent } from './componentes/data-binding/data-binding.component';
+import { InputPropertyComponent } from './componentes/input-property/input-property.component';
+import { MeuFormModule } from './componentes/meu-form/meu-form.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, DataBindingComponent, InputPropertyComponent],
+  imports: [BrowserModule, MeuFormModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
