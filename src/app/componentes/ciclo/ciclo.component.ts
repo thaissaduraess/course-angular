f751d0bd65a4edab95b583872d4301cff5215e8d
thaissa <thaissaduraes67@gmail.com>
2023-08-23 06:18:31 -0300

    colocando todas as functions de ciclo de vida angular

--- src/app/componentes/ciclo/ciclo.component.ts
+++ src/app/componentes/ciclo/ciclo.component.ts
@@ -3,13 +3,46 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ciclo',
   templateUrl: './ciclo.component.html',
-  styleUrls: ['./ciclo.component.css']
+  styleUrls: ['./ciclo.component.css'],
 })
 export class CicloComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.log('constructor');
   }
 
+  ngOnChanges() {
+    this.log('ngOnChanges');
+  }
+
+  ngOnInit() {
+    this.log('ngOnInit');
+  }
+
+  ngDoCheck() {
+    this.log('ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    this.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    this.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    this.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    this.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    this.log('ngOnDestroy');
+  }
+
+  private log(hook: string) {
+    console.log(hook);
+  }
 }
